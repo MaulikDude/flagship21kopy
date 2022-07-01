73b54bc015e5b81a0011c91f5a6410fab21e59eb
MaulikDude <95739867+MaulikDude@users.noreply.github.com>
2022-07-01 16:24:22 +0530

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -8,11 +8,15 @@ BOOTLOADER = stm32-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+ENCODER_ENABLE = yes        # rotary knob encoders
+ENCODER_MAP_ENABLE = yes
+OLED_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
