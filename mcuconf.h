8e00006c3941d47388641bcdc8ec185d5b1deb73
MaulikDude <95739867+MaulikDude@users.noreply.github.com>
2022-07-02 20:06:23 +0530

    Update mcuconf.h

--- mcuconf.h
+++ mcuconf.h
@@ -8,9 +8,8 @@
 
 #undef STM32_I2C_USE_I2C1
 #define STM32_I2C_USE_I2C1 TRUE
-#undef STM32_DAC_USE_DAC1_CH1
-#define STM32_DAC_USE_DAC1_CH1              TRUE
-#undef STM32_DAC_USE_DAC1_CH2
-#define STM32_DAC_USE_DAC1_CH2              TRUE
-#undef STM32_GPT_USE_TIM6
-#define STM32_GPT_USE_TIM6                 TRUE
+
+#undef STM32_PWM_USE_TIM4
+#define STM32_PWM_USE_TIM4                  TRUE
+#undef STM32_GPT_USE_TIM1
+#define STM32_GPT_USE_TIM1                  TRUE
