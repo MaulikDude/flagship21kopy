4ba2da58d795fc98107333c33bb6bf1a6f4119e7
MaulikDude <95739867+MaulikDude@users.noreply.github.com>
2022-07-11 22:38:03 +0530

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -15,13 +15,16 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGB_MATRIX_ENABLE = WS2812
-RGBLIGHT_ENABLE = no
+WS2812_DRIVER = pwm
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = yes           # Audio output
 AUDIO_DRIVER = pwm_software
 ENCODER_ENABLE = yes        # rotary knob encoders
 ENCODER_MAP_ENABLE = yes
 OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
 CIRQUE_PINNACLE_ENABLE_CURSOR_GLIDE_ENABLE = no
 CIRQUE_PINNACLE_ENABLE_CIRCULAR_SCROLL_ENABLE  = no
+
